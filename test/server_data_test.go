0c053fef0f141ed135905d72527511f70fb6f15b
josediaz16 <jldiaz1612@gmail.com>
2019-06-05 09:14:59 -0500

    adds server ok case to tests

--- test/server_data_test.go
+++ test/server_data_test.go
@@ -18,17 +18,19 @@ func getServerResponse(fixtureFile string) ([]byte) {
   return byteValue
 }
 
-func TestGetServerDataIsOk(t *testing.T) {
-  mockedResponse := getServerResponse("fixtures/ssllabs_down_server.json")
+func mockRequest(fixtureFile string) (*httptest.Server) {
+  mockedResponse := getServerResponse(fixtureFile)
 
-  server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+  return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
     rw.Write([]byte(mockedResponse))
   }))
+}
 
+func TestGetServerDataServerDown(t *testing.T) {
+  server := mockRequest("fixtures/ssllabs_down_server.json")
   defer server.Close()
 
   apiClient := api.API{server.Client(), server.URL}
-
   data := servers.GetServerData(&apiClient, "google.com")
 
   if len(data.Servers) != 2 {
@@ -68,6 +70,53 @@ func TestGetServerDataIsOk(t *testing.T) {
 
   if !data.IsDown {
     t.Errorf("TestGetServerDataIsOk(google.com) got IsDown false; should be true")
+  }
+}
+
+func TestGetServerDataServerOk(t *testing.T) {
+  server := mockRequest("fixtures/ssllabs_server_ok.json")
+  defer server.Close()
+
+  apiClient := api.API{server.Client(), server.URL}
+  data := servers.GetServerData(&apiClient, "google.com")
+
+  if len(data.Servers) != 2 {
+    t.Errorf("TestGetServerDataIsOk(google.com) got %d servers; should be 2", len(data.Servers))
+  }
+
+  server1 := data.Servers[0]
+  server2 := data.Servers[1]
+
+  expectedServer1 := servers.Server{
+    Address: "172.217.5.110",
+    SslGrade: "A",
+    Status: "Ready",
+    Country: "US",
+    Owner:  "Google",
+  }
+
+  expectedServer2 := servers.Server{
+    Address: "2607:f8b0:4005:808:0:0:0:200e",
+    SslGrade: "A",
+    Status: "Ready",
+    Country: "US",
+    Owner: "Google",
+  }
+
+  if server1 != expectedServer1 {
+    t.Errorf("TestGetServerDataIsOk(google.com) got %v server 1; should be %v", server1, expectedServer1)
+  }
+
+  if server2 != expectedServer2 {
+    t.Errorf("TestGetServerDataIsOk(google.com) got %v server 2; should be %v", server2, expectedServer2)
+  }
+
+  if data.Title != "Google" {
+    t.Errorf("TestGetServerDataIsOk(google.com) got title %v; should be Google", data.Title)
+  }
+
+  if data.IsDown {
+    t.Errorf("TestGetServerDataIsOk(google.com) got IsDown true; should be false")
   }
 }
 
