7d463bcbecde64f5afc2895d7331d8d3114eb057
josediaz16 <jldiaz1612@gmail.com>
2019-06-09 23:55:00 -0500

    adds searchbox component

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Server ID Service"/>
+    <SearchBox plh="Type a server domain"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SearchBox  from './components/SearchBox.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    SearchBox
   }
 }
 </script>
@@ -21,8 +24,13 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+
+}
+
+#app img {
+  width: 300px;
 }
 </style>
---
+++ front/src/components/SearchBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search-box">
+    <input v-model="searchText" type="search" :placeholder="plh"/>
+    <button v-on:click="searchServer">Search</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'SearchBox',
+  data: () => {
+    return {
+      searchText: ''
+    }
+  },
+  props: {
+    plh: String,
+  },
+  methods: {
+    searchServer: function() {
+      axios.get("http://localhost:3000/domains/search", {params: {domainName: this.searchText}})
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error))
+    }
+  }
+}
+</script>
